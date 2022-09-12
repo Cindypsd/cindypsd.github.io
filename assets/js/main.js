92e1dfbe74cd8ee4e15dcb09c549a83aa5d9fec8
Cindypsd <cindyposada3@gmail.com>
2022-09-12 19:19:03 -0400

    animaciones jbt

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+const imgJbt = document.getElementById("img-jbt");
+
+function cambiarImagen() {
+  document.getElementById("img-jbt").src = "./assets/images/fotoCindy_Azul.png";
+}
+
+function regresarImagen() {
+  document.getElementById("img-jbt").src = "./assets/images/fotoCindy.png";
+}
