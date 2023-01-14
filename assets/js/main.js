2577310261cc0ffd28226f917ac23c18794afde5
Cindypsd <cindyposada3@gmail.com>
2023-01-14 17:26:26 -0600

    nuevo diseno

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-const imgJbt = document.getElementById("img-jbt");
-
-function cambiarImagen() {
-  document.getElementById("img-jbt").src = "./assets/images/fotoCindy_Azul.png";
-}
-
-function regresarImagen() {
-  document.getElementById("img-jbt").src = "./assets/images/fotoCindy.png";
-}
